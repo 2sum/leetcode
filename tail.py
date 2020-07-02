d4fafbb484355ef431a8455983d442839d72ae76
2 sum <perfnews@gmail.com>
2020-07-01 20:17:46 -0500

    adding tail

--- tail.py
+++ tail.py
@@ -8,20 +8,30 @@ class Solution(object):
         stat_old = os.stat(fl)
         stat_old_mtime = stat_old.st_mtime
         while True:
+            print("where am i:",r.tell())
             line=r.readline()
+            r.seek(0,os.SEEK_END)
             stat = os.stat(fl)
             stat_mtime = stat.st_mtime
             if stat_mtime<=stat_old_mtime:
-                time.sleep(0.1)
+                time.sleep(1)
+                prev_seek=r.tell()
                 continue
-            stat_old_mtime=stat_mtime
-            yield line
+            else:
+                r.seek(prev_seek+1,0)
+                print("TIMES:",stat_old_mtime,stat_mtime)
+                line=r.readline()
+                print("where am i after seek:",r.tell())
+                r.seek(0,os.SEEK_END)
+                stat_old_mtime=stat_mtime
+                prev_seek=r.tell()
+                yield line
         
         #print(stat)
         #return stat.st_mtime
 
 s=Solution()
-fl="/Users/malaybiswal/Downloads/misc.txt"
+fl="/Users/malaybiswal/Downloads/a.txt"
 with open(fl,'r') as r:
     loglines = s.tail(fl,r)
     for line in loglines:
