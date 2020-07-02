7e9dc0a56a808698018d48a0361a82bf726e98ea
2 sum <perfnews@gmail.com>
2020-07-02 02:37:25 -0500

    fixed bug in tail

--- tail.py
+++ tail.py
@@ -8,7 +8,8 @@ class Solution(object):
         stat_old = os.stat(fl)
         stat_old_mtime = stat_old.st_mtime
         while True:
-            print("where am i:",r.tell())
+            line=""
+            #print("where am i:",r.tell())
             line=r.readline()
             r.seek(0,os.SEEK_END)
             stat = os.stat(fl)
@@ -18,10 +19,13 @@ class Solution(object):
                 prev_seek=r.tell()
                 continue
             else:
-                r.seek(prev_seek+1,0)
-                print("TIMES:",stat_old_mtime,stat_mtime)
-                line=r.readline()
-                print("where am i after seek:",r.tell())
+                r.seek(0,os.SEEK_END)
+                end=r.tell()
+                r.seek(prev_seek,0)
+                while r.tell()<end:
+                    #print("TIMES:",stat_old_mtime,stat_mtime)
+                    line+=r.readline()
+                    #print("where am i after seek:",r.tell())
                 r.seek(0,os.SEEK_END)
                 stat_old_mtime=stat_mtime
                 prev_seek=r.tell()
@@ -31,7 +35,7 @@ class Solution(object):
         #return stat.st_mtime
 
 s=Solution()
-fl="/tmp/Downloads/a.txt"
+fl="/Users/malaybiswal/Downloads/a.txt"
 with open(fl,'r') as r:
     loglines = s.tail(fl,r)
     for line in loglines:
