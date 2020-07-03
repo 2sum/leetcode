11b7e9eb85f4df285a17c7b7401999a9ea94c40e
2 sum <perfnews@gmail.com>
2020-07-03 02:09:47 -0500

    commiting tail -n

--- tail-n.py
+++ tail-n.py
@@ -1,7 +1,7 @@
 import os
 class Solution(object):
     def tail(self,fname,n):
-        chunksize=4
+        chunksize=4096
         remaining=0
         cur_pos=0
         i=0
@@ -11,6 +11,18 @@ class Solution(object):
             r.seek(0,os.SEEK_END)
             remaining=r.tell()
             cur_pos=r.tell()-chunksize
+            if cur_pos<=0:
+                chunksize=remaining
+                cur_pos=remaining
+                r.seek(0,0)
+                chunk=r.read(chunksize)
+                chunks=chunk.splitlines()
+                for j in range(len(chunks)-1,len(chunks)-n-1,-1):
+                    s=chunks[j]+'\n'+s
+                return s.strip()
+
+                
+
             r.seek(cur_pos,0)
             while  n>0:
                 i+=1
@@ -22,9 +34,21 @@ class Solution(object):
                     chunk=chunks[-1]
                 s=chunk+s
                 remaining -= len(chunk)
-                r.seek(remaining-(chunksize),0)
-                cur_pos=cur_pos-len(chunk)
+                
+                if cur_pos-len(chunk)>0:
+                    cur_pos=cur_pos-len(chunk)
+                    r.seek(remaining-(chunksize),0)
+                elif cur_pos-len(chunk)<=0:
+                    r.seek(0,0)
+                    overflow=cur_pos-len(chunk)# extra going negative crossing 0 seek
+                    chunk=r.read(chunksize+overflow)
+                    s=chunk+s
+                    break
+                    #cur_pos=cur_pos-len(chunk)
+        
+            
         return s
+            
 s=Solution()
 fl=input("Enter File Name:")
 n=input("Enter tail count:")
