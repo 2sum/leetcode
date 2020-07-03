e73b38444eaea0a8652c94a23a418aa4177923d5
2 sum <perfnews@gmail.com>
2020-07-02 23:19:33 -0500

    commiting tail -n

--- tail-n.py
+++ tail-n.py
@@ -11,10 +11,8 @@ class Solution(object):
             r.seek(0,os.SEEK_END)
             remaining=r.tell()
             cur_pos=r.tell()-chunksize
-            print(r.tell(),cur_pos)
             r.seek(cur_pos,0)
             while  n>0:
-                print("-------------------")
                 i+=1
                 chunk = r.read(chunksize)
                 n -= chunk.count(os.linesep)
@@ -22,25 +20,12 @@ class Solution(object):
                     chunks=chunk.splitlines()
                     l=len(chunks)
                     chunk=chunks[-1]
-                print("n:",n)
                 s=chunk+s
-                print(s,i)
                 remaining -= len(chunk)
                 r.seek(remaining-(chunksize),0)
                 cur_pos=cur_pos-len(chunk)
-        
-            
-        print("====================OUTPUT================")
         return s
-            
-
-
 s=Solution()
 fl="/Users/malaybiswal/Downloads/a.txt"
 n=3
-
 print(s.tail(fl,n))
-#with open(fl,'r') as r:
-    #loglines = s.tail(fl,n)
-    #for line in loglines:
-    #   print(line.strip())
